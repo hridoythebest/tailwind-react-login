d2147012c0fc8c401a7c4fcafefca23e775d0a81
hridoythebest <hridoythebest@gmail.com>
2024-02-08 06:27:49 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Login from './components/Login'
+import Login2 from './components/Login2'
+import Login3 from './components/Login3';
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Login2 />
+      <Login3 />
+    </div>
+  );
+}
+
+export default App;
